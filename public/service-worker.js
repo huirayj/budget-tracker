e5079ba51c6f143e5233090f07d22afa85291c05
Justin Hui <huirayj@gmail.com>
2021-07-11 16:34:44 -0400

    renamed styles to style

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,38 +1,38 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/assets/css/style.css",
-    "/assets/js/index.js",
-    "/assets/js/db.js",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-512x512.png",
-    "/manifest.webmanifest",
-    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+    '/',
+    '/index.html',
+    '/manifest.webmanifest',
+    '/assets/css/style.css',
+    '/assets/js/index.js',
+    '/assets/js/db.js',
+    '/assets/images/icons/icon-192x192.png',
+    '/assets/images/icons/icon-512x512.png',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
 ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 // install service worker
-self.addEventListener("install", (e) => {
+self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
-            console.log("Your files were pre-cached successfully!");
             return cache.addAll(FILES_TO_CACHE);
+            console.log('Your files were pre-cached successfully!');
         })
     );
     self.skipWaiting();
 });
 
 // activate service worker
-self.addEventListener("activate", (e) => {
+self.addEventListener('activate', (e) => {
     e.waitUntil(
         caches.keys().then(keyList => {
             return Promise.all(
                 keyList.map(key => {
                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Removing old cache data", key);
+                        console.log('Removing old cache data', key);
                         return caches.delete(key);
                     }
                 })
@@ -43,8 +43,8 @@ self.addEventListener("activate", (e) => {
 });
 
 // fetch information from cache
-self.addEventListener("fetch", (e) => {
-    if (e.request.url.includes("/api/")) {
+self.addEventListener('fetch', (e) => {
+    if (e.request.url.includes('/api/')) {
         console.log('[Service Worker] Fetch(data)', e.request.url);
         e.respondWith(
             caches
